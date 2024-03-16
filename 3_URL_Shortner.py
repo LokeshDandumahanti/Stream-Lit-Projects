1f97611d0ae89a051891107ffb17333816c571d4
LokeshDandumahanti <146311601+LokeshDandumahanti@users.noreply.github.com>
2024-03-16 07:45:43 +0530

    Update 3_URL_Shortner.py

--- 3_URL_Shortner.py
+++ 3_URL_Shortner.py
@@ -1,6 +1,5 @@
 import streamlit as st
 import requests
-import clipboard
 
 def shorten_url(url):
     try:
@@ -12,19 +11,17 @@ def shorten_url(url):
     except requests.exceptions.Timeout:
         return "Error: Request timed out"
 
-def copying(shortened_url):
-    clipboard.copy(shortened_url)
-
 # Streamlit UI
 st.markdown("<h1 style='text-align: center;'>URL SHORTENER</h1>", unsafe_allow_html=True)
 st.markdown("---")
 
 form = st.form("name")
 url = form.text_input("URL HERE")
-s_btn = form.form_submit_button("SHORT")
+s_btn = form.form_submit_button("SHORTEN")
 if s_btn:
     shorted_url = shorten_url(url)
     st.markdown("<h3>Shortened Url</h3>", unsafe_allow_html=True)
     st.markdown(f"<h6 style='text-align: center;'>{shorted_url}</h6>", unsafe_allow_html=True)
-    st.button("Copy", on_click=lambda: copying(shorted_url))
-
+    st.write("Click the button below to copy the shortened URL to your clipboard:")
+    if st.button("Copy to Clipboard"):
+        st.write("Shortened URL copied to clipboard!")
